360c90b1b7f5af84b9bf3fde61c4c4a03c3ff7b1
Yan Ren <yannyren@gmail.com>
2018-01-07 18:54:08 +0000

    server & router working now

--- client/src/app.js
+++ client/src/app.js
@@ -3,6 +3,11 @@ const AjaxRequest = require('./services/ajax_request.js');
 
 const app = function(){
   console.log("Hello world")
+
+  const papapigButton = document.querySelector('#papapig-button');
+  papapigButton.addEventListener('click', )
+  const papapigView = new PapapigView();
+
 }
 
 
