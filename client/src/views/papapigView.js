b6c9ae0a4d9dde821d2777023f3a756985991352
Yan Ren <yannyren@gmail.com>
2018-01-08 17:10:17 +0000

    google map working

--- client/src/views/papapigView.js
+++ client/src/views/papapigView.js
@@ -1,14 +1,25 @@
 const AjaxRequest = require('../services/ajax_request.js');
 const requestPaPaAPI = new AjaxRequest("http://localhost:3000/api/papapig");
+const MapWrapper = require('../modules/googlemap.js');
 
 const PapapigView = function() {
 
 }
 
-PapapigView.prototype.render = function(city) {
-  
-    requestPaPaAPI.get(function(allCities) {
-      console.log(allCities);
+PapapigView.prototype.render = function() {
+
+    requestPaPaAPI.get(function(city) {
+      console.log(city);
+
+      const mainRenderDiv = document.getElementById('#render_area');
+      const divCityName = document.createElement('h3')
+      const divMap = document.createElement('div');
+      const divCityVideo = document.createElement('div');
+
+      mainRenderDiv.appendChild(divCityName);
+      mainRenderDiv.appendChild(divMap);
+      mainRenderDiv.appendChild(divCityVideo);
+
       // const ul = document.querySelector('#quotes');
       //   const li = document.createElement('li');
       //   const text = document.createElement('p');
