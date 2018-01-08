4dc2fccb2244fde23b2ce63d74ea166df623aaa0
Laurence <ljwoodward.code@gmail.com>
2018-01-08 12:57:51 +0000

    added an entry to the mamapig database

--- db/mamapig.js
+++ db/mamapig.js
@@ -1,3 +1,15 @@
 use peppa_pigs;
 
 db.animals.remove({});
+
+db.animals.insert(
+  [
+    {
+      "name":"Candy Cat",
+      "type":"Cat",
+      "ppImage":"../client/build/images/Candy_Cat.png",
+      "realImage":"../client/build/images/real_cat.jpg",
+      "sound":"../client/build/sounds/cat_meow.wav"
+    }
+  ]
+)
