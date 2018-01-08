2926ffcdd3bf0b8ac7a6b6d838b1b03677b79c7e
Laurence <ljwoodward.code@gmail.com>
2018-01-08 15:28:24 +0000

    more data for db/mamapig.js

--- db/mamapig.js
+++ db/mamapig.js
@@ -10,6 +10,41 @@ db.animals.insert(
       "ppImage":"../client/build/images/Candy_Cat.png",
       "realImage":"../client/build/images/real_cat.jpg",
       "sound":"../client/build/sounds/cat_meow.wav"
-    }
+    },
+    {
+      "name":"Danny Dog",
+      "type":"Dog",
+      "ppImage":"../client/build/images/Danny_Dog.jpg",
+      "realImage":"../client/build/images/real_dog.jpg",
+      "sound":"../client/build/sounds/dog_woof.wav"
+    },
+    {
+      "name":"3",
+      "type":"",
+      "ppImage":"",
+      "realImage":"",
+      "sound":""
+    },
+    {
+      "name":"4",
+      "type":"",
+      "ppImage":"",
+      "realImage":"",
+      "sound":""
+    },
+    {
+      "name":"5",
+      "type":"",
+      "ppImage":"",
+      "realImage":"",
+      "sound":""
+    },
+    {
+      "name":"6",
+      "type":"",
+      "ppImage":"",
+      "realImage":"",
+      "sound":""
+    },
   ]
 )
