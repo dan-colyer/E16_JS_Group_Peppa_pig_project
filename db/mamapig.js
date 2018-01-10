854b879aacde430d0035ef1b246bec350fbedc6e
Laurence <ljwoodward.code@gmail.com>
2018-01-10 10:07:47 +0000

    updated mamapig api

--- db/mamapig.js
+++ db/mamapig.js
@@ -15,44 +15,50 @@ db.animals.insert(
     {
       "name":"Danny Dog",
       "type":"Dog",
-      "ppImage":"images/mamapig/ppImages/Danny_Dog.jpg",
-      "realImage":"images/mamapig/realImages/dog.jpg",
-      "sound":"sounds/dog.wav"
+      "ppImage":"images/mamapig/ppImages/Danny_Dog.png",
+      "realImage":"images/mamapig/realImages/dog.png",
+      "sound":"sounds/dog.wav",
+      "button":"images/mamapig/buttons/dog.png"
     },
     {
       "name":"Dr Brown Bear",
-      "type":"Brown Bear",
-      "ppImage":"images/mamapig/ppImages/dr_brown_bear.jpg",
+      "type":"Bear",
+      "ppImage":"images/mamapig/ppImages/Dr_Brown_Bear.png",
       "realImage":"images/mamapig/realImages/brown_bear.jpg",
-      "sound":"sounds/bear.wav"
+      "sound":"sounds/bear.wav",
+      "button":"images/mamapig/buttons/bear.png"
     },
     {
       "name":"Emily Elephant",
       "type":"Elephant",
-      "ppImage":"images/mamapig/ppImages/Emily_elephant.jpg",
+      "ppImage":"images/mamapig/ppImages/Emily_elephant.png",
       "realImage":"images/mamapig/realImages/elephant.png",
-      "sound":"sounds/elephant.wav"
+      "sound":"sounds/elephant.wav",
+      "button":"images/mamapig/buttons/elephant.png"
     },
     {
       "name":"Mr Bull",
       "type":"Bull",
       "ppImage":"images/mamapig/ppImages/mr_bull.jpg",
       "realImage":"images/mamapig/realImages/bull.jpg",
-      "sound":"sounds/cow.wav"
+      "sound":"sounds/cow.wav",
+      "button":"images/mamapig/buttons/cow.png"
     },
     {
       "name":"Mummy Pig",
       "type":"Pig",
       "ppImage":"images/mamapig/ppImages/mummy_pig.png",
-      "realImage":"images/mamapig/realImages/pig.png",
-      "sound":"sounds/pig.wav"
+      "realImage":"images/mamapig/realImages/pig.jpg",
+      "sound":"sounds/pig.wav",
+      "button":"images/mamapig/buttons/pig.jpeg"
     },
     {
       "name":"Suzy Sheep",
       "type":"Sheep",
       "ppImage":"images/mamapig/ppImages/suzy_sheep.png",
       "realImage":"images/mamapig/realImages/sheep.jpg",
-      "sound":"sounds/sheep.wav"
+      "sound":"sounds/sheep.wav",
+      "button":"images/mamapig/buttons/sheep.png"
     }
   ]
 )
